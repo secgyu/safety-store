e473ab939db7e982dae4f2ccd364b2c1e0102f9f
김규민 <abc2232002@gmail.com>
2025-10-21 23:50:25 +0900

    chore: msw 세팅팅

--- src/main.tsx
+++ src/main.tsx
@@ -5,8 +5,22 @@ import "./index.css";
 
 import App from "./App.tsx";
 
-createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
-);
+async function enableMocking() {
+  if (process.env.NODE_ENV !== "development") {
+    return;
+  }
+
+  const { worker } = await import("./mocks/browser");
+
+  // `worker.start()` returns a Promise that resolves
+  // once the Service Worker is up and ready to intercept requests.
+  return worker.start();
+}
+
+enableMocking().then(() => {
+  createRoot(document.getElementById("root")!).render(
+    <StrictMode>
+      <App />
+    </StrictMode>
+  );
+});
